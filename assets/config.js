7ddec242ae72cb47cb904d150f0dadf5c1375952
t12n13 <tousei1213@gmail.com>
2025-10-07 06:37:57 +0900

    Update config.js
    APIを設定

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 // assets/config.js
 // ★重要★ APIキーは「HTTPリファラー制限」を必ず設定してください（あなたのサイトURL）。
 const CONFIG = {
-  API_KEY: "", // ← ここに制限済みAPIキーを貼る（例: AIzaSyXXXX...）
+  API_KEY: "AIzaSyBbPlCU-0EDo_zds_f2s_sl8YSfCRdEbmA", // ← ここに制限済みAPIキーを貼る（例: AIzaSyXXXX...）
   CAL_ID: "duramoon.chill@gmail.com" // ← ご指定のカレンダーID（公開に設定しておく）
 };
