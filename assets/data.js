8c31df54b11a8e308bbe391b77c40b11f7eb577b
t12n13 <tousei1213@gmail.com>
2025-10-07 07:14:40 +0900

    Update data.js
    公式と死喰ちるの枠線のカラーを修正

--- assets/data.js
+++ assets/data.js
@@ -1,8 +1,8 @@
 window.MEMBERS = [
-  {display:"ドゥラ・ムーン公式", aka:"公式", avatar:"image/duramoon.jpg", x:"Duramoon__info", ig:"duramoon__info", tiktok:"duramoon_idol", tag:"#ドゥラムーン", color:"black"},
+  {display:"ドゥラ・ムーン公式", aka:"公式", avatar:"image/duramoon.jpg", x:"Duramoon__info", ig:"duramoon__info", tiktok:"duramoon_idol", tag:"#ドゥラムーン", color:"gray"},
   {display:"御伽 望愛", aka:"おとぎ のあ", avatar:"image/noa.jpg", x:"noa__dura", ig:"_1984180", tiktok:"n0q1_o14", tag:"#見てるよのあち", color:"hotpink"},
   {display:"終宵 ねお", aka:"おしまい ねお", avatar:"image/neo.jpg", x:"neo__dura", ig:"ponkotsuneotaro_", tiktok:"ponkotsuneotaro", tag:"#終宵ﾉ世界ﾃﾞ共依存", color:"red"},
   {display:"琥珀 トワ", aka:"こはく とわ", avatar:"image/towa.jpg", x:"towa__dura", ig:"15milk01c", tiktok:"tow4_nyan", tag:"#永遠威艶狼卍會", color:"orange"},
-  {display:"死喰 ちる", aka:"しくい ちる", avatar:"image/chill.jpg", x:"chill__dura", ig:"xx_nyqn2_xx", tiktok:"xx_nyan2_xx", tag:"#ちると堕ちる", color:"gray"},
+  {display:"死喰 ちる", aka:"しくい ちる", avatar:"image/chill.jpg", x:"chill__dura", ig:"xx_nyqn2_xx", tiktok:"xx_nyan2_xx", tag:"#ちると堕ちる", color:"white"},
   {display:"髏黯 メア", aka:"ろあん めあ", avatar:"image/mea.jpg", x:"mea__dura", ig:"roamea_", tiktok:"roamea_", tag:"#メアの瞳の中", color:"purple"}
 ];
