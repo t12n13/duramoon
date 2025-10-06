15ffe6836a3ac0e0155658ebc64d5ae780d00edb
t12n13 <tousei1213@gmail.com>
2025-10-07 07:11:30 +0900

    Update data.js
    SNSタブが表示されないバグを修正するためコードを改善

--- assets/data.js
+++ assets/data.js
@@ -1,5 +1,4 @@
-
-const MEMBERS = [
+window.MEMBERS = [
   {display:"ドゥラ・ムーン公式", aka:"公式", avatar:"image/duramoon.jpg", x:"Duramoon__info", ig:"duramoon__info", tiktok:"duramoon_idol", tag:"#ドゥラムーン", color:"black"},
   {display:"御伽 望愛", aka:"おとぎ のあ", avatar:"image/noa.jpg", x:"noa__dura", ig:"_1984180", tiktok:"n0q1_o14", tag:"#見てるよのあち", color:"hotpink"},
   {display:"終宵 ねお", aka:"おしまい ねお", avatar:"image/neo.jpg", x:"neo__dura", ig:"ponkotsuneotaro_", tiktok:"ponkotsuneotaro", tag:"#終宵ﾉ世界ﾃﾞ共依存", color:"red"},
