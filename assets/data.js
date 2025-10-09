957523b5a710748716520df9bc35b170c77db0b0
t12n13 <tousei1213@gmail.com>
2025-10-10 08:29:06 +0900

    Update data.js
    公式にtimetreeの欄を追加

--- assets/data.js
+++ assets/data.js
@@ -1,5 +1,5 @@
 window.MEMBERS = [
-  {display:"ドゥラ・ムーン公式", aka:"公式", avatar:"image/duramoon.jpg", x:"Duramoon__info", ig:"duramoon__info", tiktok:"duramoon_idol", tag:"#ドゥラムーン", color:"gray"},
+  {display:"ドゥラ・ムーン公式", aka:"公式", avatar:"image/duramoon.jpg", x:"Duramoon__info", ig:"duramoon__info", tiktok:"duramoon_idol", tag:"#ドゥラムーン",timetree: "https://timetreeapp.com/public_calendars/duramoon_live" color:"gray"},
   {display:"御伽 望愛", aka:"おとぎ のあ", avatar:"image/noa.jpg", x:"noa__dura", ig:"_1984180", tiktok:"n0q1_o14", tag:"#見てるよのあち", color:"hotpink"},
   {display:"終宵 ねお", aka:"おしまい ねお", avatar:"image/neo.jpg", x:"neo__dura", ig:"ponkotsuneotaro_", tiktok:"ponkotsuneotaro", tag:"#終宵ﾉ世界ﾃﾞ共依存", color:"red"},
   {display:"琥珀 トワ", aka:"こはく とわ", avatar:"image/towa.jpg", x:"towa__dura", ig:"15milk01c", tiktok:"tow4_nyan", tag:"#永遠威艶狼卍會", color:"orange"},
