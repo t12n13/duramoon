91ec82440030bca837d7661832de81c03d361b03
t12n13 <tousei1213@gmail.com>
2025-10-10 00:09:24 +0900

    Update config.js
    APIキーを更新

--- assets/config.js
+++ assets/config.js
@@ -2,5 +2,5 @@
 // ★重要★ APIキーは「HTTPリファラー制限」を必ず設定してください（あなたのサイトURL）。
 const CONFIG = {
   API_KEY: "AIzaSyBbPlCU-0EDo_zds_f2s_sl8YSfCRdEbmA", // ← ここに制限済みAPIキーを貼る（例: AIzaSyXXXX...）
-  CAL_ID: "37d51022e6b268f1250c4f840ffe1765d9fc27c204e3301e57772dc27d74a5b1@group.calendar.google.com" // ← ご指定のカレンダーID（公開に設定しておく）
+    CAL_ID: "37d51022e6b268f1250c4f840ffe1765d9fc27c204e3301e57772dc27d74a5b1@group.calendar.google.com" // ← ご指定のカレンダーID（公開に設定しておく）
 };
