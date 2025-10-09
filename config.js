526bdaa07d24dfb5a2090ec8993fe65214fd02c2
t12n13 <tousei1213@gmail.com>
2025-10-10 02:40:28 +0900

    Update config.js
    コードを修正

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // assets/config.js
 // ★重要★ APIキーは「HTTPリファラー制限」を必ず設定してください（あなたのサイトURL）。
 // ここに *制限済み* のAPIキーを貼ってください。
-const CONFIG = {
+window.CONFIG = {
   API_KEY: "", // ← あとで貼る（例: "AIzaSy...."）
   CAL_ID: "duramoon.chill@gmail.com" // ← ご指定のカレンダーID
 };
